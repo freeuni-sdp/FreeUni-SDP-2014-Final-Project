4f9fd8499cdf52b4b53e15f1eed868e8088b605f
eleneakhvlediani <eakhv11@freeuni.edu.ge>
2014-06-30 22:38:57 +0400

    #25 created CancelMessagelistener

---
+++ server/src/main/java/ge/edu/freeuni/taxi/dispatcher/CancelMessageListener.java
@@ -0,0 +1,13 @@
+package ge.edu.freeuni.taxi.dispatcher;
+
+import ge.edu.freeuni.taxi.core.Message;
+
+public class CancelMessageListener implements IncomingMessageListener{
+
+	@Override
+	public void onIncomingMessage(Message message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
