a47095cb2afa473d40704ec5a9963d3c4a68b043
giorgi-matiashvili <gmati11@freeuni.edu.ge>
2014-07-01 06:14:09 +0400

    #26 create and save order after receiving a message from twitter post

---
+++ server/src/main/java/ge/edu/freeuni/taxi/dispatcher/OrderMessageListener.java
@@ -0,0 +1,28 @@
+package ge.edu.freeuni.taxi.dispatcher;
+
+import java.util.Date;
+
+import ge.edu.freeuni.taxi.Passenger;
+import ge.edu.freeuni.taxi.PassengerOrder;
+import ge.edu.freeuni.taxi.core.Message;
+import ge.edu.freeuni.taxi.core.MessageType;
+import ge.edu.freeuni.taxi.manager.OrderManager;
+
+public class OrderMessageListener implements IncomingMessageListener{
+
+	@Override
+	public void onIncomingMessage(Message message) {
+		if(message.getMessageType() == MessageType.CLIENT_ORDERED){
+			// TODO
+			OrderManager manager = OrderManager.getInstance();
+			PassengerOrder order = new PassengerOrder();
+			order.setCreateTime(new Date());
+			Passenger p = new Passenger();
+			p.setName(message.getSender());
+			order.setPassenger(p);
+			manager.updateOrder(order);
+		}
+		
+	}
+
+}
