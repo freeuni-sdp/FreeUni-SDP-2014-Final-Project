d8ddee019519e9d836cff3f843a8f308c2b65170
vatm1010@gmail.com <vatm1010@gmail.com>
2014-07-02 23:36:54 +0400

    #27 changed test drivers

--- console/src/main/java/ge/edu/freeuni/taxi/rest/DriverRestService.java
+++ console/src/main/java/ge/edu/freeuni/taxi/rest/DriverRestService.java
@@ -15,22 +15,29 @@ import java.util.List;
 @Path("/drivers")
 public class DriverRestService {
 
+    private static List<Driver> list = new ArrayList<>();
+
+    static {
+        list.add(new Driver("driverone", new Location("Kandelaki Street", System.currentTimeMillis())));
+        list.add(new Driver("drivertwo", new Location("Irakli Abashidze Street", System.currentTimeMillis())));
+        list.add(new Driver("driverthree", new Location("Budapest Street #13a", System.currentTimeMillis())));
+        list.add(new Driver("driverfour", new Location("Leselidze St", System.currentTimeMillis())));
+        list.add(new Driver("driverfive", new Location("Rustaveli", System.currentTimeMillis())));
+    }
+
     @Path("/")
     @GET
     @Consumes(MediaType.APPLICATION_JSON)
     public List<Driver> getDrivers() {
         System.out.println("all drivers");
-        List<Driver> list = new ArrayList<>();
-        list.add(new Driver("qochi", new Location("prochi", System.currentTimeMillis())));
-        list.add(new Driver("vato", new Location("mawkulane", System.currentTimeMillis())));
-        list.add(new Driver("cxondo", new Location("pachkorteeee", System.currentTimeMillis())));
+
         return list;
     }
 
     @Path("/{name}")
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
-    public boolean updateDriver( @PathParam("name") String name) {
+    public boolean updateDriver(@PathParam("name") String name) {
         System.out.println("add drivereeee with name " + name);
         return false;
     }
