e657f48792ff974d803bb59a0a77b689e1bcd519
eleneakhvlediani <eakhv11@freeuni.edu.ge>
2014-07-06 16:23:43 +0400

    #25 CancelMessageListener onIncommingMessage implementation

--- server/src/main/java/ge/edu/freeuni/taxi/dispatcher/CancelMessageListener.java
+++ server/src/main/java/ge/edu/freeuni/taxi/dispatcher/CancelMessageListener.java
@@ -1,12 +1,21 @@
 package ge.edu.freeuni.taxi.dispatcher;
 
+
+import ge.edu.freeuni.taxi.PassengerOrder;
 import ge.edu.freeuni.taxi.core.Message;
+import ge.edu.freeuni.taxi.manager.OrderManager;
 
 public class CancelMessageListener implements IncomingMessageListener{
 
 	@Override
 	public void onIncomingMessage(Message message) {
-		// TODO Auto-generated method stub
+
+		OrderManager orderManager = OrderManager.getInstance();
+		String passengerName = message.getSender();
+		PassengerOrder order = orderManager.getOrderByPassengerName(passengerName);
+		if(order!=null) orderManager.deleteOrder(order);
+		
+		
 		
 	}
 
