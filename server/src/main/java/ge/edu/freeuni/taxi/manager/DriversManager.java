4c541cb934b513940cd1b58e00cd66bde9a40454
eleneakhvlediani <eakhv11@freeuni.edu.ge>
2014-07-07 01:18:36 +0400

    #25 added method updateDriverAvailability

--- server/src/main/java/ge/edu/freeuni/taxi/manager/DriversManager.java
+++ server/src/main/java/ge/edu/freeuni/taxi/manager/DriversManager.java
@@ -58,6 +58,16 @@ public class DriversManager {
         em.getTransaction().commit();
         return driver;
     }
+    public Driver updateDriverAvailability(long id, boolean available) {
+    	 em.getTransaction().begin();
+         Driver driver = em.find(Driver.class, id);
+         driver.setAvailable(available);
+         em.merge(driver);
+         em.getTransaction().commit();
+         return driver;
+    }
+    
+    
 
     public Driver updateDriverLocation(long id, Location location) {
         em.getTransaction().begin();
