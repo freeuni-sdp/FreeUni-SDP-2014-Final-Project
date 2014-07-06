b72f59c8238b84f067f8030eb4960ce997368cc0
eleneakhvlediani <eakhv11@freeuni.edu.ge>
2014-07-07 01:18:36 +0400

    #25 added method updateDriverAvailability

--- server/src/main/java/ge/edu/freeuni/taxi/dispatcher/CancelMessageListener.java
+++ server/src/main/java/ge/edu/freeuni/taxi/dispatcher/CancelMessageListener.java
@@ -1,8 +1,12 @@
 package ge.edu.freeuni.taxi.dispatcher;
 
 
+import java.sql.DriverManager;
+
+import ge.edu.freeuni.taxi.Driver;
 import ge.edu.freeuni.taxi.PassengerOrder;
 import ge.edu.freeuni.taxi.core.Message;
+import ge.edu.freeuni.taxi.manager.DriversManager;
 import ge.edu.freeuni.taxi.manager.OrderManager;
 
 public class CancelMessageListener implements IncomingMessageListener{
@@ -13,9 +17,12 @@ public class CancelMessageListener implements IncomingMessageListener{
 		OrderManager orderManager = OrderManager.getInstance();
 		String passengerName = message.getSender();
 		PassengerOrder order = orderManager.getOrderByPassengerName(passengerName);
-		if(order!=null) orderManager.deleteOrder(order);
+		if(order!=null)  orderManager.deleteOrder(order);
 		
+		DriversManager driverManager = DriversManager.getInstance();
 		
+		Driver d  = order.getDriver();
+		driverManager.updateDriverAvailability(d.getId(), true);
 		
 	}
 
