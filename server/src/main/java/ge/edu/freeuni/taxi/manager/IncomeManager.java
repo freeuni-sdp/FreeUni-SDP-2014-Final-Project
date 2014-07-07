69d15f5f19821df74f314f07d029f7e9595c9a59
nodarius <n0dari@yahoo.com>
2014-07-07 21:35:58 +0400

    #20 add driver income manager and pojo for that income

---
+++ server/src/main/java/ge/edu/freeuni/taxi/manager/IncomeManager.java
@@ -0,0 +1,64 @@
+package ge.edu.freeuni.taxi.manager;
+
+import ge.edu.freeuni.taxi.Driver;
+import ge.edu.freeuni.taxi.DriverIncome;
+import ge.edu.freeuni.taxi.PassengerOrder;
+import ge.edu.freeuni.taxi.db.EMFactory;
+
+import javax.persistence.EntityManager;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class IncomeManager {
+
+	private static IncomeManager instance;
+
+	public static IncomeManager getInstance() {
+		if (instance == null) {
+			instance = new IncomeManager();
+		}
+		return instance;
+	}
+
+	private EntityManager em;
+
+	private IncomeManager() {
+		em = EMFactory.createEM();
+	}
+
+	public List<DriverIncome> getDriversIncome() {
+		List<DriverIncome> incomes = new ArrayList<>();
+
+		List<Driver> drivers = em.createQuery("SELECT d FROM Driver d", Driver.class).getResultList();
+
+		for (Driver driver : drivers) {
+			List<PassengerOrder> orders = em.createQuery("SELECT o FROM PassengerOrder o WHERE o.driver.id = :driverId", PassengerOrder.class)
+					.setParameter("driverId", driver.getId()).getResultList();
+
+			double income = 0;
+			for (PassengerOrder order : orders) {
+				income += order.getAmount();
+			}
+
+			DriverIncome driverIncome = new DriverIncome();
+			driverIncome.setDriver(driver);
+			driverIncome.setAmount(income);
+
+			incomes.add(driverIncome);
+		}
+
+		Collections.sort(incomes, new Comparator<DriverIncome>() {
+			@Override
+			public int compare(DriverIncome o1, DriverIncome o2) {
+				if (o1 == null || o1 == null) {
+					return 0;
+				}
+				return (int) (o1.getAmount() - o2.getAmount());
+			}
+		});
+
+		return incomes;
+	}
+}
