401d1db29105f459070381d8bc2edea167aaca1b
defaultcoder1 <giorgits@yahoo.com>
2014-07-12 01:12:11 +0400

    #29 add driver

--- console/src/main/webapp/scripts/controllers/DriverController.js
+++ console/src/main/webapp/scripts/controllers/DriverController.js
@@ -16,6 +16,7 @@
 
           $scope.driverForm = {name: ''};
           DriverFactory.getDrivers().then(function(res) {
+              $scope.drivers = res.data;
               res.data.forEach(function(driver) {
                   $('.driversT tbody').append('<tr>'+
                       '<td class="nameC" align="middle"><input class="form-control" type="text" spellcheck="false" autocomplete="off" value="'+ driver.name +'" /></td>'+
@@ -27,7 +28,18 @@
           });
 
           $scope.addDriver = function() {
-
+            var driver = {available: true, id: '', lastWorkingDate: '', location: {}, locationLastUpdateTime: '', name: $scope.driverForm.name, working: false};
+            DriverFactory.add(driver).then(function(res) {
+                var driver = res.data;
+                console.log(driver);
+                $('.driversT tbody').append('<tr>'+
+                    '<td class="nameC" align="middle"><input class="form-control" type="text" spellcheck="false" autocomplete="off" value="'+ driver.name +'" /></td>'+
+                    '<td class="incomeC" align="middle"><input class="form-control" type="text" spellcheck="false" autocomplete="off" value="'+ driver.income +'" /></td>'+
+                    '<td class="editC" align="center" valign="middle"><button class="btn btn-primary">Edit</button></td>'+
+                    '<td class="deleteC" align="center" valign="middle"><button class="btn btn-danger">Delete</button></td>'+
+                    '</tr>');
+                $('.add_driver_input').val('');
+            });
           }
     });
 })();
